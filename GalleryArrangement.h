a3e6288e1474ec02d3062cbfa50a00dbc956e6ad
doctorbatmangod <dehority14s@ncssm.edu>
2013-09-28 17:27:10 -0400

    added gallerydatafile

---
+++ GalleryArrangement.h
@@ -0,0 +1,20 @@
+#ifndef ARRANGEMENT_H_ADDED
+#define ARRANGEMENT_H_ADDED
+
+typedef struct wall{
+	int x_pos; //define x position of the initial point of the wall
+	int y_pos;
+	int angle; //angle from 0 to 180 of the child, with 0 directly up
+        struct wall* child; 
+} wall;
+
+//we don't know how many walls we'll have, so we use a linked list of them
+typedef struct wallList{
+	wall* value;
+	struct wallList* next;
+} wallList;
+
+typedef struct arrangement{
+	wallList* walls;
+	int cameraPattern; // we have a finite number of camera patterns we're testing
+} arrangement;
