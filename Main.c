596bdfd5b35351211b2c13ed7d81fcef91b10217
doctorbatmangod <dehority14s@ncssm.edu>
2013-09-30 22:56:59 -0400

    fixed occasional segfault in changeWall

--- Main.c
+++ Main.c
@@ -8,8 +8,9 @@ int main(void) {
 	srand(time(NULL));
 	arrangement *a = createRandomArrangement();
 	printArrangement(a);
-	//mutate(a);
-	//printArrangement(a);
+	int i;
+	mutate(a);
+	printArrangement(a);
 	freeArrangement(a);
 
 	//printWall(randomWall(1 + rand() % 4, 22.0* (double)rand()/(double)RAND_MAX, 20.0* (double)rand()/(double)RAND_MAX, 0.0));
